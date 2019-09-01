fa9a6b01b089960c51f0fe1d06463467a289c6c4
MahmoudSaad_ZeroX86 <msammamsa10@gmail.com>
2019-09-01 17:00:26 +0200

    SDK_Re-designed

---
+++ OLD_SDK/MSA_TivaC_SDK/MSA_ECU_LAYER/layer_info.h
@@ -0,0 +1,13 @@
+/*
+ * layer_content.h
+ *
+ *  Created on: Aug 20, 2019
+ *      Author: msams
+ */
+
+#ifndef MSA_ECU_LAYER_LAYER_INFO_H_
+#define MSA_ECU_LAYER_LAYER_INFO_H_
+
+
+
+#endif /* MSA_ECU_LAYER_LAYER_INFO_H_ */
